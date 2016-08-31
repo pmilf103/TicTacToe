8728add4f6795f0e046485c50fd5ed4dca6eff8f
pmilf103 <patrick.milford3@gmail.com>
2016-08-30 19:57:48 -0600

    Round 2
    Corrected hasWon and reset functionality.

--- TicTacToeScript.js
+++ TicTacToeScript.js
@@ -1,58 +1,107 @@
 $(document).ready(function() {
 
-	var empty = "empty";									//Define Classes
+	var empty = "empty";									//Selectors
 	var cross = "cross";
 	var circle = "circle";
-	var selected = "selected";
-	var turn = "cross";										//Set Turn
-	//var board = [	[$('tl'), $('tc'), $('tr')],		
-	//				[$('ml'), $('mc'), $('mr')],		
-	//				[$('bl'), $('bc'), $('br')]];
+	var turn = "cross";	
+	var button = $('button');
+	var board = [	[$('#tl'), $('#tc'), $('#tr')],		
+					[$('#ml'), $('#mc'), $('#mr')],		
+					[$('#bl'), $('#bc'), $('#br')]		];									
 	
 	$('td').click(function(){
-		
-		if($(this).hasClass(empty)){
+		var td = $(this);	
+
+		if(td.hasClass(empty)){								
 			
-			$(this).removeClass(empty);
-			$(this).addClass(turn);
-			
-			if(turn == "cross") {							//X's Turn										
-				$(this).text("X");
-				turn = "circle";							//Alternate Player	
-			}
-			
-			else {											//O's Turn
-				$(this).text("O");
-				turn = "cross";								//Alternate Player	
-			}
+			if(turn == cross) 								//Place X							
+				td.text("X");
+			else 											//Place O
+				td.text("O");	
+
+			td.removeClass(empty);							//Change class
+			td.addClass(turn);
+				
+			if(hasWon())									//Declare if winner
+				showWinner();
+
+			alternate();
 		}
+
 		else
 			alert("This space has been claimed");			//Occupied space
-
-		if(hasWon()){										//Declare winner
-			if(turn == "cross")
-				alert("The Xs have won it");			
-			else
-				alert("The Os have won it");
-		}
+			
 	});
 
+	$('button').click(function(){
+		reset();
+	});
+
+	
 	function hasWon(){
-		/*for(var i = 0; i < 3; i++){
-			if(	
-				($(board[i][0]).hasClass(turn) 	&& 	$(board[i][1]).hasClass(turn) 	&& 	$(board[i][2]).hasClass(turn))		|| 	//Horizontal
-				($(board[0][i]).hasClass(turn) 	&& 	$(board[1][i]).hasClass(turn) 	&& 	$(board[2][i]).hasClass(turn) 		)	//Vertical
-					return true;
+		for(var i = 0; i < 3; i++){
+			
+			if(board[i][0].hasClass(turn)	&& 	board[i][1].hasClass(turn) 	&& 	board[i][2].hasClass(turn))		//Horizontal
+				return true
+			else if(board[0][i].hasClass(turn) && 	board[1][i].hasClass(turn) 	&& 	board[2][i].hasClass(turn))	//Vertical
+				return true;
 		}
-			else if(
-				($(board[0][0]).hasClass(turn) 	&& 	$(board[1][1]).hasClass(turn) 	&& 	$(board[2][2]).hasClass(turn)) 	||		//Diagonal
-				($(board[0][2]).hasClass(turn) 	&& 	$(board[1][1]).hasClass(turn) 	&& 	$(board[2][0]).hasClass(turn)) 	)
-					return true;
 		
-			else
-				return false;*/																													//No Win
+		if(board[0][0].hasClass(turn) && 	board[1][1].hasClass(turn) 	&& 	board[2][2].hasClass(turn))			//Diagonal
+			return true;
+		else if(board[0][2].hasClass(turn) && 	board[1][1].hasClass(turn) 	&& 	board[2][0].hasClass(turn)) 				
+			return true;	
+		
+		return false;																							//No win
+		
 	}
-		
+	
+	
+	function reset(){
+		for(var i = 0; i < 3; i++){
+			for(var j = 0; j < 3; j++){
+				
+				if(board[i][j].hasClass(circle))
+					board[i][j].removeClass(circle);
+	
+				else if(board[i][j].hasClass(cross))
+					board[i][j].removeClass(cross);
+					
+				board[i][j].addClass(empty);
+				board[i][j].text(" ");
+			}	
+		}	
+	}	
+
+	function alternate(){
+		if(turn == cross) 															
+			turn = circle;
+		else 										
+			turn = cross;	
+	}
+
+	function showWinner(){
+		if(turn == cross)
+			winner = "X";
+		else
+			winner = "O";
+
+		for(var i = 0; i < 3; i++){
+			for(var j = 0; j < 3; j++){
+
+				if(board[i][j].hasClass(circle))
+					board[i][j].removeClass(circle);
+				else if(board[i][j].hasClass(cross))
+					board[i][j].removeClass(cross);
+				else 
+					board[i][j].removeClass(empty);
+				
+				board[i][j].addClass(turn);
+				board[i][j].text(winner);
+			}	
+		}
+	}
+
 });	
 
 
